4789ad34da068f22ad3e6bee6e8af7e9634af26e
Emerson <emersonpedrosacontato@gmail.com>
2024-06-21 16:15:17 -0300

    Api funcionando

--- Controller/Api_Gem.js
+++ Controller/Api_Gem.js
@@ -1,7 +1,8 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI('AIzaSyCmOD7c_zGMVLX70XwNHWLNSrMwce_RVOQ');
+const genAI = new GoogleGenerativeAI(process.env.api_key);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
